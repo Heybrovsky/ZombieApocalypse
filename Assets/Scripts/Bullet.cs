a741f3d0c3978c4cf0f61ab2c7aac992db9ba200
Marceli <marcelikusmierski@gmail.com>
2022-01-24 23:56:13 +0100

    Fixing errors

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -11,30 +11,4 @@ public class Bullet : MonoBehaviour
     {
         GetComponent<Renderer>().material.color = Color.black;
     }
-
-  /*void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.tag == "Enemy")
-        {
-            Destroy(this.gameObject);
-            healthCur = bulletDamage - other.gameObject.GetComponent<EnemySpawn>.enemyHealth;
-            if (healthCur <= 0)
-            {
-                Destroy(other.gameObject);
-            }
-        }
-    }
-
-
-    void Update()
-    {
-        
-
-    }
-
-    void bulletAttack()
-    {
-        Debug.Log("ShouldBeDmgd");
-        enemy.enemyHealth = enemy.enemyHealth - bulletDamage;
-    }*/
 }
